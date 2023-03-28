d979b2ce14a9b65b8b2a51493db013e865bb94c5
Andres <aeum3893@gmail.com>
2023-03-28 19:11:15 -0400

    table for samplepack show

--- app/javascript/controllers/audio_player_controller.js
+++ app/javascript/controllers/audio_player_controller.js
@@ -4,10 +4,12 @@ import { Howl, Howler } from "howler"
 // Connects to data-controller="audio-player"
 export default class extends Controller {
   static values = { sample: String }
-  static targets = [ "button" ]
+  static targets = [ "button", "time" ]
   sound;
 
   connect() {
+    this.timeTarget.textContent = "Loading..."
+
     this.sound = new Howl({
       src: [this.sampleValue],
       onplay: () => {
@@ -17,7 +19,6 @@ export default class extends Controller {
                               <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
                             </svg>`
         let analyser = Howler.ctx.createAnalyser()
-        console.log(analyser)
       },
       onstop: () => {
         const button = this.buttonTarget
@@ -28,6 +29,13 @@ export default class extends Controller {
       },
       onend: () => {
         this.sound.stop()
+      },
+      onload: () => {
+        const seconds = Math.floor(this.sound._duration);
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        const timeString = `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
+        this.timeTarget.textContent = timeString;
       }
     })
   }
